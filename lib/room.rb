488d388c135da450e526a1fcd8195d9c3e5b87e8
DreamzRunzLA <salmaan.a.qadir@gmail.com>
2019-07-30 23:39:36 -0600

    Finished part 1

--- lib/room.rb
+++ lib/room.rb
@@ -1,3 +1,37 @@
 class Room
+    def initialize(capacity)
+        @capacity = capacity
+        @occupants = []
+    end
 
+    def capacity
+        @capacity
+    end
+
+    def occupants
+        @occupants
+    end
+
+    def full?
+        if self.occupants.length < self.capacity
+            return false
+        else
+            return true
+        end
+    end
+
+    def available_space
+        return self.capacity - self.occupants.length
+    end
+
+    def add_occupant(name)
+        if self.full?
+            return false
+        else
+            self.occupants << name
+            return true
+        end
+    end
+
+    
 end
